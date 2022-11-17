19c9a4bc6bb212d7afeee0112ece2cd761c4c192
Aravind104 <ontagodiaravind@gmail.com>
2022-11-17 12:24:10 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Q1:What is the typeof []
+// console.log(typeof []);
+// O:object
+// Exp: Array is basically a object
+
+// Q2:
+function sample(){
+    const a=b=c=1;
+    console.log(typeof a,b,c)
+}
+
+sample();
